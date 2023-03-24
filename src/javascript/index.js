552c676417001c4e1327edf361cc35b103b3cd7e
Ahmet Ufuk Kurt <ahmetufuk159@gmail.com>
2023-03-25 02:52:53 +0300

    Homepage

---
+++ src/javascript/index.js
@@ -0,0 +1,31 @@
+//* close notification bar
+const closebtn = document.querySelector(".notification img");
+const notification = document.querySelector(".notification");
+closebtn.addEventListener("click", () => {
+  notification.style.display = "none";
+});
+
+//* slider photo in body
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+}

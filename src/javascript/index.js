ac6c2e6c6a6e20ee49075bd311fafdaed18a3736
Ahmet Ufuk Kurt <ahmetufuk159@gmail.com>
2023-04-12 20:53:34 +0300

    first page responsive

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -29,3 +29,11 @@ function showSlides(n) {
 
   slides[slideIndex - 1].style.display = "block";
 }
+
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navigation")[0];
+const newLinks = document.getElementsByClassName("responsive")[0];
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+  newLinks.classList.toggle("active");
+});
